7c4f9f8e6a003a6a738ecc6de4c61c186f6bb5fc
trestes <mistermaxpeck@gmail.com>
2023-05-31 11:59:11 -0700

    add boilerplate.js

---
+++ boilerplate.js
@@ -0,0 +1,24 @@
+//REQUIRE FS//
+
+const fs = require('fs');
+
+//FOLDER NAME ARGUMENT PLUS DEFAULT OF PROJECT//
+
+const folderName = process.argv[2] || 'Project';
+
+//CREATE FOLDER SKELETON//
+
+try {
+    fs.mkdirSync(folderName);
+    fs.mkdirSync(`${folderName}/css`);
+    fs.mkdirSync(`${folderName}/js`);
+
+    //CREATE FILE SKELETON//
+
+    fs.writeFileSync(`${folderName}/index.html`, '');
+    fs.writeFileSync(`${folderName}/css/styles.css`, '');
+    fs.writeFileSync(`${folderName}/js/script.js`, '');
+} catch (e) {
+    console.log("You have met with a dark fate.");
+    console.log(e);
+}
